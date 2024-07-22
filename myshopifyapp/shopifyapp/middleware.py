343476d0d66d2547f124eb89547d086acbd5b55f
unknown <kashish>
2024-07-22 13:25:43 +0530

    updates to iframe issue

--- myshopifyapp/shopifyapp/middleware.py
+++ myshopifyapp/shopifyapp/middleware.py
@@ -2,10 +2,10 @@ from django.utils.deprecation import MiddlewareMixin
 
 class XFrameOptionsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        response['X-Frame-Options'] = 'ALLOW-FROM https://quickstart-3aa8ad1d.myshopify.com'
+        response['X-Frame-Options'] = 'ALLOW-FROM https://*.myshopify.com'
         return response
 
 class ContentSecurityPolicyMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        response['Content-Security-Policy'] = "frame-ancestors 'self' https://quickstart-3aa8ad1d.myshopify.com"
+        response['Content-Security-Policy'] = "frame-ancestors 'self' https://*.myshopify.com https://admin.shopify.com"
         return response
